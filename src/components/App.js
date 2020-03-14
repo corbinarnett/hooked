c77c156182702202405210691cf0763ec53eb007
corbinarnett <corbin.c.arnett@gmail.com>
2020-03-14 11:42:35 -0700

    change api key

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import Movie from "./Movie";
 import Search from "./Search";
 
 
-const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=4a3b711b"; // you should replace this with yours
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=2683adcb"; // you should replace this with yours
 
 
 const App = () => {
